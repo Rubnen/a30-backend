8b566da48664fa75b7eb947deff93ac23578205d
Ruben Manuel Pereira Da Silva <121290791+Rubnen@users.noreply.github.com>
2025-05-19 16:14:51 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,8 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, FieldValue } from "firebase-admin/firestore";
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+
 initializeApp({
   credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT),
 });
